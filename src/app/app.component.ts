45af7797df684d05025844056f855dd6f67579f4
nabendu82 <nabendu.biswas@gmail.com>
2022-06-30 20:37:16 +0530

    Part 1

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('f') signupForm: NgForm;
+  defaultQuestion = 'teacher';
+  answer = '';
+  genders = ['male', 'female']
+
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+  }
+
+  onSubmit(){
+    console.log(this.signupForm)
+  }
+}
