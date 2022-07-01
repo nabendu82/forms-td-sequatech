9d714b546c77c77f13809a5a4957303946d122ab
nabendu82 <nabendu.biswas@gmail.com>
2022-07-01 20:50:27 +0530

    Final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,12 +11,29 @@ export class AppComponent {
   defaultQuestion = 'teacher';
   answer = '';
   genders = ['male', 'female']
+  user = {
+    username: '',
+    email: '',
+    secret: '',
+    questionAnswer: '',
+    gender: ''
+  }
+  submitted = false;
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    this.signupForm.form.patchValue({
+      username: suggestedName
+    })
   }
 
   onSubmit(){
-    console.log(this.signupForm)
+    this.submitted = true;
+    this.user.username = this.signupForm.value.username;
+    this.user.email = this.signupForm.value.email;
+    this.user.secret = this.signupForm.value.secret;
+    this.user.questionAnswer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.signupForm.reset();
   }
 }
